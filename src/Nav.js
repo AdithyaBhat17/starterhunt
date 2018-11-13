4c110d93d5c2a94c3bef3ef88d6f8f8315bb2e20
AdithyaBhat17 <adithyabhat17.ab@gmail.com>
2018-11-13 18:20:30 +0530

    search bar

---
+++ src/Nav.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export default function Nav(props) {
+    const { search } = props
+    return(
+        <nav className="navbar navbar-default">
+            <div className="container">
+                <div className="navbar-header">
+                    <a className="navbar-brand" href="#S">StarterHub</a>
+                </div>
+                <form className="navbar-form navbar-left">
+                    <div className="form-group">
+                        <input
+                         type="text" 
+                         className="form-control" 
+                         placeholder="Search for projects" 
+                         onChange={(event) => search(event)}
+                        />
+                    </div>
+                </form>
+            </div>
+        </nav>
+    )
+}
